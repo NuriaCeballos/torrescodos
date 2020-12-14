352cafbc277ee472a2a57f43ea6da9b2a2f69699
pgmez92 <72202803+pgmez92@users.noreply.github.com>
2020-12-15 00:12:13 +0100

    actualizacion_motorclave
    -ahora puedes elegir dificultad (a través de una ventana modal)

--- js/motor_clave.js
+++ js/motor_clave.js
@@ -4,32 +4,42 @@ let torre3 = document.querySelector(".torre3")
 
 let nube = document.querySelector(".nube");
 
-let dificultad = 8;
+let dificultad = 4;
+let alto = 30;
+let espacio = ((dificultad + 1) * alto);
 
-let puntuacion = document.querySelector(".puntuacion");
-let jugador = document.querySelector(".jugador");
+let jugador_puntuacion = JSON.parse(sessionStorage.getItem("truco")).puntuacion;
+document.querySelector(".puntuacion").innerHTML = jugador_puntuacion;
+document.querySelector(".jugador").innerHTML = JSON.parse(sessionStorage.getItem("truco")).nombre;
 
-for (let i = 0; i < dificultad; i++) {
-    let disco = document.createElement("div")
-    disco.classList.add("disco");
-    disco.classList.add("no-click");
-    disco.innerHTML = i;
-    disco.value = i;
+function crearTorres() {
+    let ancho = 25;
 
-    torre1.appendChild(disco)
+    for (let i = 0; i < dificultad; i++) {
+        let disco = document.createElement("div")
+        disco.classList.add("disco");
+        disco.classList.add("no-click");
+        disco.innerHTML = i;
+        disco.value = i;
+        disco.style.width = ancho + "%";
+        ancho = ancho + 15;
+        torre1.appendChild(disco)
+        colorinesHanoi();
+    }
+
+    torre1.style.height = espacio;
+    torre2.style.height = espacio;
+    torre3.style.height = espacio;
+    torre1.style.paddingTop = espacio - (torre1.children.length) * alto + "px";
+
+    torre1.value = torre1.firstChild.value;
+    torre2.value = 110;
+    torre3.value = 110;
 }
 
-let ancho = 15;
+crearTorres();
 
-torre1.querySelectorAll(".disco").forEach((disco) => {
-    disco.style.width = ancho + "%";
-    ancho = ancho + 15;
-})
-
-torre1.value = torre1.firstChild.value;
-torre2.value = 110;
-torre3.value = 110;
-
+//el juego en un click
 document.querySelector(".hanoi").onclick = (e) => {
     if (nube.firstChild == null) {
         if (e.target == torre1 || e.target == torre2 || e.target == torre3) {
@@ -39,16 +49,20 @@ document.querySelector(".hanoi").onclick = (e) => {
                 alert("Coge una ficha primero, so zote!")
             }
             if (e.target.children.length > 0) {
-                e.target.value = e.target.firstChild.value
+                e.target.value = e.target.firstChild.value;
+                e.target.style.paddingTop = espacio - (e.target.children.length) * alto + "px";
             } else {
                 e.target.value = 110;
+                e.target.style.paddingTop = espacio - (e.target.children.length) * alto + "px";
             }
         }
     } else {
         if (nube.firstChild.value < e.target.value) {
             e.target.insertAdjacentElement("afterbegin", nube.firstChild);
             e.target.value = e.target.firstChild.value;
-            puntuacion.innerHTML++;
+            e.target.style.paddingTop = espacio - (e.target.children.length) * alto + "px";
+            ++jugador_puntuacion;
+            document.querySelector(".puntuacion").innerHTML = jugador_puntuacion;
             hasGanado();
         } else {
             alert("El tamaño SÍ importa..")
@@ -56,13 +70,21 @@ document.querySelector(".hanoi").onclick = (e) => {
     }
 }
 
+//comprueba la victoria
+function hasGanado() {
+    if (torre3.children.length == dificultad) {
+        alert("Has ganado!");
+    }
+}
+
+//truqui 
 function codos() {
-    let discos=document.querySelectorAll(".disco");
-    let ez=dificultad-1;
-    while(ez>=0){
-        discos.forEach((disco)=>{
-            if(disco.value==ez){
-                torre3.insertAdjacentElement("afterbegin",disco);
+    let discos = document.querySelectorAll(".disco");
+    let ez = dificultad - 1;
+    while (ez >= 0) {
+        discos.forEach((disco) => {
+            if (disco.value == ez) {
+                torre3.insertAdjacentElement("afterbegin", disco);
                 --ez;
             }
         });
@@ -70,11 +92,52 @@ function codos() {
     hasGanado();
 }
 
-function hasGanado() {
-    if (torre3.children.length == dificultad) {
-        alert("Has ganado!");
+//funcion randomcolorz
+function colorinesHanoi() {
+    var colorDisco = document.querySelectorAll(".disco");
+    colorDisco.forEach(function (ye) {
+        var r = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+        var g = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+        var b = Math.floor(Math.random() * (255 - 0 + 1) + 0);
+        var rgbString = r + ", " + g + ", " + b;
+        ye.style.backgroundColor = 'rgb(' + rgbString + ')';
+    });
+}
+
+//cambio la dificultad
+function cambiarDificultad() {
+    let respuesta = confirm("Cambiar de dificultad reiniciará su partida, ¿desea continuar?");
+    if (respuesta != false) {
+        let btn_diff = document.getElementsByName("dificultad");
+        for (let i = 0; i < btn_diff.length; i++) {
+            if (btn_diff[i].checked != false) {
+                dificultad = parseInt(btn_diff[i].value);
+            }
+        }
+        jugador_puntuacion=0;
+        document.querySelector(".puntuacion").innerHTML = jugador_puntuacion;
+        robertOppenheimer();
+        crearTorres();
     }
 }
+
+//destruyo el juego
+function robertOppenheimer() {
+    let discos = document.querySelectorAll(".disco");
+    discos.forEach((disco) => {
+        disco.remove();
+    });
+}
+
+/*Ventana emergente version marta (reworked)*/
+function controlVentana() {
+    if (document.querySelector(".ventana").style.display == "none") {
+        document.querySelector(".ventana").style.display = "block";
+    } else {
+        document.querySelector(".ventana").style.display = "none"
+    }
+}
+
 
 /* function mariaJose (){
     funcion destinada a la humiliasion
